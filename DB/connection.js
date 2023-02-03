ead8f5b04ead6405674bb9155949da06476a0d6e
Osama Maree <108696087+osama-maree@users.noreply.github.com>
2023-02-03 23:48:28 +0200

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", true);
+const mongoose = require("mongoose");//use mongoose
+mongoose.set("strictQuery", true);//reject all text in console page
 const connectDB = async () => {
   return await mongoose
-    .connect("mongodb://localhost:27017/saraha")
+    .connect("mongodb://localhost:27017/saraha")//create connection with database
     .then((res) => {
       console.log("connection DB");
     })
